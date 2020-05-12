92f4a163412885c5c41d68297130098c3b2403fe
Jim Klassen <klassen.js@gmail.com>
2020-05-12 13:09:40 -0500

    PDAL proof-of-concept from 2019-09-09

---
+++ run-dsm.sh
@@ -0,0 +1,11 @@
+for i in ../laz/4342-0{1,2,3,4}-{26,27,28,29,30,31,32,33}_?_?.laz ; do 
+	echo Starting $i
+	sem -j+0 \
+		pdal pipeline pipeline-dsm.json \
+			--readers.las.filename=$i \
+			--writers.gdal.filename=`basename ${i%%.laz}-dsm.tif`
+done
+sem --wait
+
+echo Merging tiles...
+gdal_merge.py -tap -o dsm.tif *-dsm.tif
